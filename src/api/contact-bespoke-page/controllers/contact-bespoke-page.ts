daab67ad9b52d2e19a3c8357f1b6a1900b2ee138
Prakhar Srivastava <prakhar.srivastava040@gmail.com>
2026-07-23 12:46:16 +0530

    added gallery key in the response of bespoke page

--- src/api/contact-bespoke-page/controllers/contact-bespoke-page.ts
+++ src/api/contact-bespoke-page/controllers/contact-bespoke-page.ts
@@ -45,6 +45,9 @@ export default factories.createCoreController(
                 coverImage:{
                   populate:
                     '*'
+                },
+                gallery: {
+                  populate: '*'
                 }
               }
             },
