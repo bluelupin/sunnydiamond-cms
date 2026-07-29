5457a4919ebb743ea69a4c66e61212713724b62b
Kartik Saxena <kartiksaxena2005@gmail.com>
2026-07-29 17:50:41 +0530

    feat: add Career landing and Career listing page

---
+++ src/api/submissions-job-opening/services/submissions-job-opening.ts
@@ -0,0 +1,7 @@
+/**
+ * submissions-job-opening service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::submissions-job-opening.submissions-job-opening');
