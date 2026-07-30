387afe909e9d6c9781e268ba40f1f038cab722ab
Prakhar Srivastava <prakhar.srivastava040@gmail.com>
2026-07-30 10:47:35 +0530

    populate the policy page API response

--- src/api/policy-certifications-page/controllers/policy-certifications-page.ts
+++ src/api/policy-certifications-page/controllers/policy-certifications-page.ts
@@ -3,7 +3,35 @@
  */
 
 import { factories } from '@strapi/strapi';
+import { fullSeoPopulate } from '../../../utils/populate';
+
+const populate = {
+  headerSection: true,
+  policyCategories: {
+    populate: {
+      policies: {
+        populate: {
+          accordionItems: true,
+        },
+      },
+    },
+  },
+  contactSection: {
+    populate: {
+      contactOptions: true,
+    },
+  },
+  seo: fullSeoPopulate,
+};
 
 export default factories.createCoreController(
-  'api::policy-certifications-page.policy-certifications-page' as any
+  'api::policy-certifications-page.policy-certifications-page' as any,
+  () => ({
+    async find(ctx) {
+      if (ctx.query.populate === '*') {
+        ctx.query = { ...ctx.query, populate } as any;
+      }
+      return super.find(ctx);
+    },
+  })
 );
