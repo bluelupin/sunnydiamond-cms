93a3dc361496ca881ba827c040e27fa069692657
Prakhar Srivastava <prakhar.srivastava040@gmail.com>
2026-07-31 17:00:56 +0530

    changed path url for education page

--- src/utils/frontend-revalidation.ts
+++ src/utils/frontend-revalidation.ts
@@ -40,7 +40,7 @@ const REVALIDATION_MAPPINGS: Record<string, RevalidationMapping> = {
   },
   'api::learn-about-diamonds-page.learn-about-diamonds-page': {
     tags: ['cms:learn-about-diamonds-page'],
-    paths: ['/education'],
+    paths: ['/learn-about-diamonds'],
   },
   'api::product-landing-page.product-landing-page': {
     tags: ['cms:product-landing-page'],
