a7e391ac9e895dbe03051f5148410f0806b68c7e
Prakhar Srivastava <prakhar.srivastava040@gmail.com>
2026-08-21 15:52:59 +0530

    fixed the PDP API controller

--- src/api/product-display-page/controllers/product-display-page.ts
+++ src/api/product-display-page/controllers/product-display-page.ts
@@ -4,4 +4,29 @@
 
 import { factories } from '@strapi/strapi';
 
-export default factories.createCoreController('api::product-display-page.product-display-page');
+const wildcardPopulate = {
+  findYourSize: true,
+  stripItems: {
+    populate: {
+      icon: true,
+    },
+  },
+  stripTnc: true,
+  hereForYouCard: true,
+  personaliseCard: true,
+  pairItWith: true,
+  visitUsSection: true,
+};
+
+export default factories.createCoreController(
+  'api::product-display-page.product-display-page' as any,
+  () => ({
+    async find(ctx) {
+      if (ctx.query.populate === '*') {
+        ctx.query = { ...ctx.query, populate: wildcardPopulate } as any;
+      }
+
+      return super.find(ctx);
+    },
+  })
+);
